4f54d8091cb39afd256e33ef4b86759b9b591f36
Nick Kutryj <mikolaj.kutryj@gmail.com>
2021-05-13 21:04:08 +0200

    Bump version

--- cmd/root.go
+++ cmd/root.go
@@ -33,7 +33,7 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:     "test-results",
-	Version: "0.1.0",
+	Version: "0.2.1",
 	Short:   "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
