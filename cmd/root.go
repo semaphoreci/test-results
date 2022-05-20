fa7ffc71438bc4cd07ce4a2d2c04177d183196c8
Mikołaj Kutryj <mikolaj.kutryj@gmail.com>
2022-05-20 08:58:42 +0200

    Bumped version number to 0.5.0 (#52)

--- cmd/root.go
+++ cmd/root.go
@@ -30,7 +30,7 @@ import (
 
 var cfgFile string
 
-var versionString = "0.4.15"
+var versionString = "0.5.0"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
