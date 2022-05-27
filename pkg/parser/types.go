7c4254985513297778a86f175733bd593f94d8d9
Mikołaj Kutryj <mikolaj.kutryj@gmail.com>
2022-05-27 09:36:43 +0000

    chore: cleanup TODOs

--- pkg/parser/types.go
+++ pkg/parser/types.go
@@ -39,7 +39,6 @@ const (
 )
 
 // Result ...
-// [TODO] Better name is required...
 type Result struct {
 	TestResults []TestResults `json:"testResults"`
 }
@@ -316,7 +315,6 @@ func (me *Suite) hasTest(test Test) bool {
 }
 
 // Aggregate all tests in suite
-// TODO: add flag to skip aggregating already present data
 func (me *Suite) Aggregate() {
 	summary := Summary{}
 
