fc9ccc344ece710c49bdf32e81dc35633c24bf7d
Mikołaj Kutryj <mikolaj.kutryj@gmail.com>
2023-08-01 22:04:17 +0200

    fix: add missing flags from compile cmd (#63)

--- cmd/compile.go
+++ cmd/compile.go
@@ -107,5 +107,7 @@ var compileCmd = &cobra.Command{
 }
 
 func init() {
+	compileCmd.Flags().Int32P("trim-output-to", "s", 0, "trim stdout to N characters, defaults to 0(unlimited)")
+	compileCmd.Flags().BoolP("omit-output-for-passed", "o", false, "omit stdout if test passed, defaults to false")
 	rootCmd.AddCommand(compileCmd)
 }
